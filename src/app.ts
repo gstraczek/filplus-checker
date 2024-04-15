03eddee48950c907d3d72a2571b736dfda5db6c7
Will Scott <will.scott@protocol.ai>
2024-04-15 10:42:19 +0200

    don't think issues are prs

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ const handler: ApplicationFunction = (app: Probot, _options: ApplicationFunction
       throw new Error('Invalid environment variable CRITERIA')
     }
     const checker = getCidChecker(app.log.child({ contextId: context.id }))
-    if (context.pullRequest != undefined) {
+    if (context.payload.action === 'created' && context.name !== "issues") {
       const pre : PullRequestReviewCommentCreatedEvent = context.payload as PullRequestReviewCommentCreatedEvent
 
       const result = await checker.checkFromPR(pre, criteria, otherAddresses)
